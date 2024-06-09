ad4f69d5688231f58f6cdc16bcfd3bcde214b047
NunezGabriel <gabriel.nunez.arenas@gmail.com>
2024-06-09 18:55:15 -0500

    ultimos cambios

--- globalResources.py
+++ globalResources.py
@@ -4,15 +4,17 @@ def menu():
     print("*------------------------------------------------------*")
     print("| - Elije la opcion que quieras utilizar               |")
     print("|                                                      |")
-    print("| 1) Elije la opcion que quieras utilizar              |")
-    print("| 2) Elije la opcion que quieras utilizar              |")
-    print("| 3) Elije la opcion que quieras utilizar              |")
-    print("| 4) Elije la opcion que quieras utilizar              |")
-    print("| 5) Salir                                             |")
+    print("| 1) Administrar vehiculos                             |")
+    print("| 2) Cobro de de cupo                                  |")
+    print("| 3) Mostrar Lista                                     |")
+    print("| 4) Buscar informacion del propietario                |")
+    print("| 5) Papelera                                          |")
+    print("| 6) Salir                                             |")
     print("*------------------------------------------------------*")
 
 def mensajeDespedida():
     print("*---------------------------------------------------------*")
     print("| GRACIAS POR USAR EL SISTEMA DE CONTROL, HASTA PRONTO 👋 |")
     print("*---------------------------------------------------------*")
+
 
--- main.py
+++ main.py
@@ -4,7 +4,22 @@ while True:
     menu()
     opcion = int(input("Ingrese un numero de opcion valido: "))
 
-    if opcion == 5:
+    if opcion == 1:
+        mensajeDespedida()
+        break
+    elif opcion == 2:
+        mensajeDespedida()
+        break
+    elif opcion == 3:
+        mensajeDespedida()
+        break
+    elif opcion == 4:
+        mensajeDespedida()
+        break
+    elif opcion == 5:
+        mensajeDespedida()
+        break
+    elif opcion == 6:
         mensajeDespedida()
         break
     else:
