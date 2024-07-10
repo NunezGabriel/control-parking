a8d594c523ee6a1aece36fe8ebbc718450d8e421
SherinneL <sherinne.lombardi@tecsup.edu.pe>
2024-07-10 17:28:35 -0500

    gaa

--- cobro_cupos.py
+++ cobro_cupos.py
@@ -1,54 +1,71 @@
-import datetime
+import datetime, globalResources as glbR, vehicles_admin as va
 
 diccionarioxDia = {}        # Se guardan las visitas
 historialVisitas = {}       # Para enlazar con el verdadero historial
 estadoDescuento = {}        # Si se usaron descuentos (cupos)
 
 
-def calcular_descuento(visitas):
-    return (visitas // 5) * 0.10
+def calcular_descuento(visita):         # Por cada 5 visitas, se le da un descuento del 10%
+    return (visita // 5) * 0.10
+
+
+def cobro_de_cupos():
+    print("*------------------------------------------------------*")
+    print("|                    COBRO DE CUPOS                    |")
+    print("*------------------------------------------------------*")
+    print("| - Elije la opcion que quieras utilizar               |")
+    print("|                                                      |")
+    print("| 1) Entrada                                           |")
+    print("| 2) Salida                                            |")
+    print("| 3) Descuentos                                        |")
+    print("| 4) Atrás                                             |")
+    print("*------------------------------------------------------*")
+
+
+cobro_de_cupos()
 
 
 while True:
-    En_Sa = input("1-Entrada\t2-Salida\t3-Descuentos\t4-Atrás\n--> ")
-    if En_Sa == "4":
-        break
-
+    En_Sa = input("--> ")
     cobroxHora = 2.00
     descuento = 0
+    if En_Sa == "4":            # Retrocede al menú
+        glbR.menu()
 
     if En_Sa == "1":
-        Cliente = input("Ingrese su número de DNI: ")
+        # placa = va.registrar_vehiculos()            # De vehicles_admin
+        placa = input("Ingrese la placa: ")
         Entrada = datetime.datetime.now()
-        diccionarioxDia[Cliente] = Entrada
-        if Cliente in historialVisitas:
-            historialVisitas[Cliente] += 1
+        diccionarioxDia[placa] = Entrada            # Guarda la entrada
+        if placa in historialVisitas:
+            historialVisitas[placa] += 1            # Aumenta el historial de visitas
         else:
-            historialVisitas[Cliente] = 1
-            estadoDescuento[Cliente] = "no aplicado"
+            historialVisitas[placa] = 1             # Primera visita
+            estadoDescuento[placa] = "no aplicado"  # no hay suficiente para solicitar un descuento
 
     elif En_Sa == "2":
-        buscarDNI = input("Ingrese su número de DNI: ")
-        if buscarDNI in diccionarioxDia:
-            Salida = datetime.datetime.now()
-            tiempodeEntrada = diccionarioxDia[buscarDNI]
-            tiempoTotal = Salida - tiempodeEntrada
-            totalSegundos = tiempoTotal.total_seconds()
-            totalHoras = (totalSegundos // 30)
-            cobroTotal = totalHoras * cobroxHora
+        buscarPlaca = input("Ingrese su número de placa: ")
+        if buscarPlaca in diccionarioxDia:                  # Busca la placa donde se registró
+            Salida = datetime.datetime.now()                # Guarda la salida
+            tiempodeEntrada = diccionarioxDia[buscarPlaca]  # Guarda el tiempo de entrada en otra variable
+            tiempoTotal = Salida - tiempodeEntrada          # Se calcula el tiempo que se estuvo dentro
+            totalSegundos = tiempoTotal.total_seconds()     # Se calcula el tiempo total en segundos
+            totalHoras = (totalSegundos // 30)              # Cada 30 segundos es 1 hora
+            cobroTotal = totalHoras * cobroxHora            # Se calcula cuánto corresponde cobrar por el tiempo
 
-            if buscarDNI in historialVisitas:
-                visitas = historialVisitas[buscarDNI]
-                if visitas >= 5 and estadoDescuento[buscarDNI] == "no aplicado":
-                    descuento = calcular_descuento(visitas)
+            if buscarPlaca in historialVisitas:
+                visitas = historialVisitas[buscarPlaca]         # Se saca la cantidad de visitas registrado
+                if visitas >= 5 and estadoDescuento[buscarPlaca] == "no aplicado":
+                    descuento = calcular_descuento(visitas)     # Se calcula el descuento
                     cobroTotal -= cobroTotal * descuento
-                    estadoDescuento[buscarDNI] = "aplicado"
+                    estadoDescuento[buscarPlaca] = "aplicado"   # Ya no se puede usar el descuento
 
+                    # Boleta:
             boleta = (
                 "============================\n"
                 "         BOLETA DE PAGO        \n"
                 "============================\n"
-                f"DNI del Cliente: {buscarDNI}\n"
+                f"Placa del auto: {buscarPlaca}\n"
                 f"Hora de Entrada: {tiempodeEntrada}\n"
                 f"Hora de Salida:  {Salida}\n"
                 f"Tiempo Total:    {int(totalHoras)} horas\n"
@@ -57,29 +74,32 @@ while True:
                 "============================\n"
             )
 
+            # Guarda la boleta
             archivo = open("boleta.txt", "a")
             archivo.write(boleta)
             archivo.close()
 
+            # Se imprime la boleta
             print(boleta)
-            del diccionarioxDia[buscarDNI]
+            # Se elimina el registro de entrada para una próxima
+            del diccionarioxDia[buscarPlaca]
 
         else:
             print("No ha ingresado todavía")
 
     elif En_Sa == "3":
-        DNI_descuentos = input("Ingrese su número de DNI: ")
-        if DNI_descuentos in historialVisitas:
-            num_rep_DNI = historialVisitas[DNI_descuentos]
-            if num_rep_DNI >= 5 and estadoDescuento[DNI_descuentos] == "no aplicado":
-                descuento = calcular_descuento(num_rep_DNI)
+        Placa_descuentos = input("Ingrese su número de placa: ")
+        if Placa_descuentos in historialVisitas:                        # Busca la placa en el historial
+            num_rep_Placa = historialVisitas[Placa_descuentos]          # Número de repeticiones
+            if num_rep_Placa >= 5 and estadoDescuento[Placa_descuentos] == "no aplicado":
+                descuento = calcular_descuento(num_rep_Placa)           # Calcula el descuento
                 print(f"Tiene un descuento de: {descuento * 100}%")
-            elif estadoDescuento[DNI_descuentos] == "aplicado":
+            elif estadoDescuento[Placa_descuentos] == "aplicado":
                 print("El descuento ya ha sido aplicado y no es válido nuevamente.")
             else:
                 print("No tiene suficientes visitas para un descuento")
         else:
-            print("No se encontraron registros de visitas para este DNI")
+            print("No se encontraron registros de visitas para este número de placa")
 
     else:
         print("La opción no es válida, inténtelo nuevamente")
